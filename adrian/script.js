7535297121e15004e75722ef47e2dae32dc909c0
AdrianValdes <adrianvaldeshdz@gmail.com>
2020-11-09 13:33:17 +0100

    Solving exercise 3 inside functions

--- adrian/script.js
+++ adrian/script.js
@@ -31,16 +31,28 @@ function createPattern(arr) {
 // createPattern(arr);
 
 //3
-for (let i = 1; i <= 4; i++) {
-  let n = 0;
-  while (n < 3) {
-    console.log(i);
-    n++;
+function createLine1() {
+  let line1 = "";
+  for (let i = 1; i <= 4; i++) {
+    let n = 0;
+    while (n < 3) {
+      line1 += i + " ";
+      n++;
+    }
   }
+  return line1;
 }
 
-for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < 5; j++) {
-    console.log(j);
+// console.log(createLine1());
+
+function createLine2() {
+  line2 = "";
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 5; j++) {
+      line2 += j + " ";
+    }
   }
+  return line2;
 }
+
+// console.log(createLine2());
